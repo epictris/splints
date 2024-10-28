c867be135bccca35e3aaf1f886d4d8857bec5d87
Tristan Bray <tristan@uncountable.com>
2024-10-29 00:22:42 +0100

    register missing methods

--- splints/register_methods.py
+++ splints/register_methods.py
@@ -1,9 +1,11 @@
 from splints.methods.diagnostic import diagnostic
 from splints.methods.document_changed import document_changed
 from splints.methods.document_opened import document_opened
+from splints.methods.document_closed import document_closed
 from splints.methods.initialize import initialize
 from splints.methods.initialized import initialized
 from splints.methods.shutdown import shutdown
+from splints.methods.exit import exit
 from splints.server import Server
 
 
@@ -17,4 +19,8 @@ def register_methods(server: Server):
     server.register_method(
         name=document_opened.arg_type.__name__, func=document_opened.func
     )
+    server.register_method(
+        name=document_opened.arg_type.__name__, func=document_closed.func
+    )
     server.register_method(name=shutdown.arg_type.__name__, func=shutdown.func)
+    server.register_method(name=exit.arg_type.__name__, func=exit.func)
