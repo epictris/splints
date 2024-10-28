d20c889dc05f6a37ca205ffe4e2c8131fc5098cc
Tristan Bray <tristan@uncountable.com>
2024-10-28 23:34:34 +0100

    implement diagnostic generation from linter rules

--- splinter/diagnostics.py
+++ splinter/diagnostics.py
@@ -1,31 +1,59 @@
+import os
+import fnmatch
 import re
 
-from splinter.types.shared import Diagnostic, DiagnosticSeverity, DiagnosticTag, Position, Range
+from splinter.types.linting import LintRule, Severity, TextFormat
+from splinter.types.shared import (
+    Diagnostic,
+    DiagnosticSeverity,
+    DiagnosticTag,
+    Position,
+    Range,
+    TextDocumentItem,
+)
+import urllib.parse
 
-DEPRECATED_PATTERNS = [
-    "deprecated",
-    "pattern",
-]
+CONVERT_SEVERITY = {
+    Severity.ERROR: DiagnosticSeverity.ERROR,
+    Severity.WARNING: DiagnosticSeverity.WARNING,
+    Severity.INFO: DiagnosticSeverity.INFO,
+    Severity.HINT: DiagnosticSeverity.HINT,
+}
+
+CONVERT_FORMAT = {
+    TextFormat.STRIKETHROUGH: {DiagnosticTag.DEPRECATED},
+    TextFormat.FADE: {DiagnosticTag.UNNECESSARY},
+}
 
 
-def generate_diagnostics(text: str) -> set[Diagnostic]:
-    lines = text.splitlines()
+def generate_diagnostics(
+    text_document: TextDocumentItem, rules: list[LintRule]
+) -> set[Diagnostic]:
+    file_path = os.path.relpath(urllib.parse.urlparse(text_document.uri).path)
+    applicable_rules = [
+        rule
+        for rule in rules
+        if any(fnmatch.fnmatch(file_path, path) for path in rule.file_globs)
+    ]
+    lines = text_document.text.splitlines()
     diagnostics: set[Diagnostic] = set()
     for lineno, line in enumerate(lines):
-        for pattern in DEPRECATED_PATTERNS:
-            matches = re.finditer(pattern, line)
+        for rule in applicable_rules:
+            matches = re.finditer(rule.pattern, line)
             for match in matches:
                 diagnostics.add(
                     Diagnostic(
                         source="splinter",
-                        severity=DiagnosticSeverity.INFO,
-                        tags=frozenset({DiagnosticTag.UNNECESSARY, DiagnosticTag.DEPRECATED}),
-                        code=f"test-deprecated-pattern-code",
+                        severity=CONVERT_SEVERITY[rule.severity],
+                        tags=frozenset(
+                            CONVERT_FORMAT[rule.format] if rule.format else set()
+                        ),
+                        code=rule.code,
                         range=Range(
                             start=Position(line=lineno, character=match.start()),
                             end=Position(line=lineno, character=match.end()),
                         ),
-                        message=f"Deprecated pattern '{pattern}'",
+                        message=rule.message,
                     )
                 )
     return diagnostics
